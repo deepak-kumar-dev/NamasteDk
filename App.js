65c307ec9bc239a5a6847734b8d554e6162e742b
deepak-kumar-dev <deepakkumarlamoria@gmail.com>
2023-12-23 08:46:14 +0530

    first commit - started basic react structure

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default  App = () => {
+  return (
+    <h1>Namaste Nikki</h1>
+  )
+}
+
+const react = ReactDOM.createRoot(document.getElementById("root"));
+
+react.render(App());
