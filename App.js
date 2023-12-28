eafca9ff9b0a056d2d2c22ba35926d556492397a
deepak-kumar-dev <deepakkumarlamoria@gmail.com>
2023-12-28 21:36:16 +0530

    Started adding components - header added

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./assets/logo.jpg";
 
-export default  App = () => {
+const Header = () => {
   return (
-    <h1>Namaste Nikki</h1>
-  )
-}
+    <header className="header">
+      <div className="logo">
+        <img className="logo-img" src={logo} alt="Company logo" />
+      </div>
+      <div className="items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default App = () => {
+  return (
+    <div className="main">
+      <Header></Header>
+    </div>
+  );
+};
 
 const react = ReactDOM.createRoot(document.getElementById("root"));
 
